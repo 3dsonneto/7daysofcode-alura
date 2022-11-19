2da0c42577777b52e3dd9c5708fd37b167c61608
Edson <edson_neto1996@hotmail.com>
2022-11-18 22:34:19 -0300

    Initial Commit

---
+++ 7daysofcode/_daysofcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _daysofcodeApp.swift
+//  7daysofcode
+//
+//  Created by Edson Neto on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct _daysofcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
